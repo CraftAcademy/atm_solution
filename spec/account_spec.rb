0ba454b044925dc43d685ca14f480f877cb08821
tochman <thomas@nocebo.se>
2016-04-22 22:37:15 +0200

    WIP Account class - added balance to account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,8 @@ describe Account do
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
   end
+
+  it 'is expected to have a balance of 0 on initialize' do
+    expect(subject.balance).to eq 0
+  end
 end
