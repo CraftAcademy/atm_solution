5c07b93c9b3820818aed6abfe8e8ba52b3ece6c3
Raoul DIFFOUO <diraulo@gmail.com>
2016-04-23 11:43:25 +0200

    Fix exception handling failing spec

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,8 @@
+require 'pry-byebug'
 class Account
   attr_accessor :pin_code, :balance, :exp_date, :account_status, :owner
   STANDARD_VALIDITY_YRS = 5
-  
+
 
   def initialize(attrs = {})
     @pin_code = generate_pin
@@ -10,10 +11,10 @@ class Account
     @account_status = :active
     set_owner(attrs[:owner])
   end
-  
+
   def self.deactivate(account)
     account.account_status = :deactivated
-  end 
+  end
 
   def deactivate
     @account_status = :deactivated
@@ -24,15 +25,15 @@ class Account
   def generate_pin
     rand(1000..9999)
   end
-  
+
   def set_exp_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime("%m/%y")
   end
-  
+
   def set_owner(obj)
     obj == nil ?  missing_owner : @owner = obj
   end
-  
+
   def missing_owner
     raise "An Account owner is required"
   end
